90fc1d2844618a7f39ef1fc276e55f0b64142558
Kirill Kuznetsov <kuznechik.86@mail.ru>
2023-07-15 01:36:18 +0300

    Add sign

--- src/main/java/ru/gransoft/SpringApplicationStart.java
+++ src/main/java/ru/gransoft/SpringApplicationStart.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author Kuznetsovka 14.07.2023
+ */
 @SpringBootApplication
 @EnableEurekaServer
 @OpenAPIDefinition
